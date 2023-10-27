28d22b77a1fd1d2bb66b70cc24e6b00e74550e08
Naga sai subba reddy <121428764+20A21A04D2@users.noreply.github.com>
2023-10-27 18:39:47 +0530

    email_conf.py

---
+++ email_conf.py
@@ -0,0 +1,6 @@
+MAILGUN_API_KEY = "37686018f4b18c018a3647de5b767004-324e0bb2-a8211119" # Present in Mailgun dashboard
+SANDBOX_URL=  "sandboxac8c17546a2841a29db7e0c88f57fa2a.mailgun.org"     # Also present in Mailgun dashboard
+SENDER_EMAIL ="test@"+"sandboxac8c17546a2841a29db7e0c88f57fa2a.mailgun.org"
+RECIPIENT_EMAIL = "subbareddymedapati60@gmail.com" # Email Id to recieve the mail
+API_KEY = "9230e25c-927e-4223-a0e2-c1b0c110ac53"         # Api key on bolt cloud
+DEVICE_ID = "BOLT9127485"       # From bolt cloud
