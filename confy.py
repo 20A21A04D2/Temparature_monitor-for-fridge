afb592df669892033eaf81ece027f1f5323d392c
Naga sai subba reddy <121428764+20A21A04D2@users.noreply.github.com>
2023-10-27 18:28:33 +0530

    confy.py

---
+++ confy.py
@@ -0,0 +1,8 @@
+SID="ACa3789d61a9571b03769a1f041e6e4882"              # Present in Twilio dashboard
+AUTH_TOKEN="5bd660cf8d54716b2fc5256a8300c02a"   # Also present in Twilio dashboard
+FROM_NUMBER="+19164361218" # The number alloted on account creation
+TO_NUMBER="+919141725777"   # Number where you want to recieve SMS
+API_KEY="9230e25c-927e-4223-a0e2-c1b0c110ac53"      # Api key on bolt cloud
+DEVICE_ID="BOLT9127485"   # From bolt cloud
+FRAME_SIZE=10           # For z-score analysis(value can be changed)
+MUL_FACTOR=           # For z-score analysis(value can be changed)
